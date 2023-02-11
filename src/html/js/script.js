5eea5ed3b9fc76936581fea51c5ec379871d954d
Venodez <venodez@gmail.com>
2023-02-11 22:38:00 +0100

    Fix of lot of things

--- src/html/js/script.js
+++ src/html/js/script.js
@@ -1,25 +1,63 @@
-const virusTotal = require('api')('@virustotal/v3.0#40nj53llc655dro');
 const AdmZip = require('adm-zip');
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 
-const analyse = (filePath) => {
-    console.log(filePath);
-    virusTotal.postFiles({file: filePath}, {accept: 'application/json', 'x-apikey': '64d8dfc2b763c37c0e8bb4c2bc9d17e5eee0aff4c92e756f29c33b25b3fa6b9e'})
-        .then((response) => {
-            console.log(response);
-            fs.unlinkSync(filePath);
-        }).catch((error) => {
-            console.log(error);
+let viruses = [];
+let browser;
+let page;
+
+(async () => {
+    browser = await puppeteer.launch({ headless: false});
+    page = await browser.newPage();
+})();
+
+const analyse = async (filePath) => {
+    return new Promise(async (resolve, reject) => {
+        await page.goto('https://virusscan.jotti.org');
+        await page.waitForSelector('input[type="file"]');
+        const fileInput = await page.$('input[type="file"]');
+        fileInput.uploadFile(filePath).then(async () => {
+            await page.waitForSelector('[class="statusText"]');
+            let result = await page.$('[class="statusText"]')
+            let text = await (await result.getProperty('textContent')).jsonValue();
+            while (!text.startsWith("Scan terminé")) {
+                result = await page.$('[class="statusText"]')
+                text = await (await result.getProperty('textContent')).jsonValue();
+            }
+            resolve(isVirus(text));
         });
+    });
+}
+
+const isVirus = (result) => {
+    let lines = result.split(' ');
+    let percentageSplit = lines[2].split('/');
+    if (percentageSplit[0] >= 7)
+        return true;
+    return false;   
+}
+
+const deleteVirues = () => {
+    for (const virus of viruses) {
+        if (fs.existsSync(virus))
+            fs.unlinkSync(virus);
+    }
 }
 
 const input = document.getElementById('folder');
 
-input.addEventListener('change', (event) => {
+input.addEventListener('change', async (event) => {
+
     for (const file of event.target.files) {
+        console.log(file.path);
         let zip = new AdmZip();
         zip.addLocalFile(file.path);
         zip.writeZip(file.path + '.zip');
-        analyse(file.path + '.zip');
+        let virus = await analyse(file.path + '.zip');
+        if (virus) {
+            viruses.push(file.path);
+            console.log("Virus detected");
+        }
+        fs.unlinkSync(file.path + '.zip');
     }
 });
